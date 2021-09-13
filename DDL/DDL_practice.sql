987ca5a84fcf61cffe130b87246eadf5d620a9fc
Yamato Tadokoro <miynevfor311jap@gmail.com>
2021-09-13 08:50:53 -0700

    second commit, adding the create and alter statements

---
+++ DDL/DDL_practice.sql
@@ -0,0 +1,45 @@
+-- (1) Create a database called movie_sample;
+CREATE DATABASE movie_sample;
+
+-- (2) Write the SQL needed to create an actors, directors, movies table from the ERD.
+CREATE TABLE actors (
+    actor_id serial PRIMARY KEY,
+    first_name VARCHAR(150),
+    last_name VARCHAR(150)
+);
+
+CREATE TABLE directors (
+    director_id serial PRIMARY KEY,
+    first_name VARCHAR(150),
+    last_name VARCHAR(150)
+);
+
+ALTER TABLE directors
+ADD COLUMN date_of_birth DATE,
+ADD COLUMN nationality VARCHAR(120),
+
+DROP TABLE movies
+
+
+CREATE TABLE movies (
+    movie_id serial PRIMARY KEY,
+    movie_name VARCHAR NOT NULL,
+    movie_length INT,
+    release_date DATE,
+	movie_lang VARCHAR(30),
+    age_certificate VARCHAR(30),
+    director_id INT REFERENCES directors(director_id)
+);
+
+
+
+ALTER TABLE movies 
+ADD COLUMN language VARCHAR(150);
+
+ALTER TABLE movies
+-- RENAME COLUMN language TO m_lang;
+ADD Constraint movie_length CHECK(movie_length > 0);
+	
+ALTER TABLE actors
+-- ADD COLUMN gender VARCHAR(10)
+ADD COLUMN date_of_birth DATE;
